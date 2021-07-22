a0ef798c2b90ba709a059668944bd689c3de890d
sudeepmamidi <mamidisudeep98@gmail.com>
2021-07-22 15:08:50 +0530

    firstComiit

---
+++ BooksApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BooksApp.Areas.Identity.Data;
+using BooksApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BooksApp.Areas.Identity.IdentityHostingStartup))]
+namespace BooksApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BooksDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BooksDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<BooksDbContext>();
+            });
+        }
+    }
+}
